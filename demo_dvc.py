b805f4842487ef93b5725d142f0cbe9bea86f36f
Agnieszka <agnieszka@pawlak.in>
2021-11-29 17:43:21 +0100

    initial commit

---
+++ demo_dvc.py
@@ -0,0 +1,79 @@
+import sys
+
+import pandas as pd
+import numpy as np
+from sklearn.metrics import mean_squared_error, mean_absolute_error, r2_score
+from sklearn.model_selection import train_test_split
+from sklearn.linear_model import ElasticNet
+from urllib.parse import urlparse
+import mlflow
+import mlflow.sklearn
+import dvc.api
+
+path = 'data/winequality-red.csv'
+repo = '/home/agnieszka/Documents/test'
+version = 'v1'
+
+data_url = dvc.api.get_url(path=path,
+                           repo=repo,
+                           rev=version)
+
+data = pd.read_csv(data_url, sep=";")
+
+print(data.head())
+
+
+
+def eval_metrics(actual, pred):
+    rmse = np.sqrt(mean_squared_error(actual, pred))
+    mae = mean_absolute_error(actual, pred)
+    r2 = r2_score(actual, pred)
+    return rmse, mae, r2
+
+# Split the data into training and test sets. (0.75, 0.25) split.
+train, test = train_test_split(data)
+
+# The predicted column is "quality" which is a scalar from [3, 9]
+train_x = train.drop(["quality"], axis=1)
+test_x = test.drop(["quality"], axis=1)
+train_y = train[["quality"]]
+test_y = test[["quality"]]
+
+alpha = float(sys.argv[1]) if len(sys.argv) > 1 else 0.5
+l1_ratio = float(sys.argv[2]) if len(sys.argv) > 2 else 0.5
+
+experiment_id = mlflow.set_experiment("Demo")
+with mlflow.start_run(experiment_id=experiment_id):
+    lr = ElasticNet(alpha=alpha, l1_ratio=l1_ratio, random_state=42)
+    lr.fit(train_x, train_y)
+
+    predicted_qualities = lr.predict(test_x)
+
+    (rmse, mae, r2) = eval_metrics(test_y, predicted_qualities)
+
+    print("Elasticnet model (alpha=%f, l1_ratio=%f):" % (alpha, l1_ratio))
+    print("  RMSE: %s" % rmse)
+    print("  MAE: %s" % mae)
+    print("  R2: %s" % r2)
+
+    mlflow.log_param("alpha", alpha)
+    mlflow.log_param("l1_ratio", l1_ratio)
+
+    mlflow.log_param("data_url", data_url)
+    mlflow.log_param("data_version", version)
+    mlflow.log_param("input_rows", data.shape[0])
+
+    mlflow.log_metric("rmse", rmse)
+    mlflow.log_metric("r2", r2)
+    mlflow.log_metric("mae", mae)
+
+    cols_x = pd.DataFrame(list(train_x.columns))
+    cols_x.to_csv('features.csv', header=False, index=False)
+    mlflow.log_artifact('features.csv')
+
+    tags = {"key 1": "value 1",
+            "key 2": "value 2"}
+
+    mlflow.set_tags(tags)
+
+    mlflow.sklearn.log_model(lr, "model")
